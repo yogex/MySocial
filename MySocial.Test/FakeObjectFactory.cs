a7f42072bda77c203813c4ae4f2fcdb391fe5ece
Benedictus Yoga <yogex@outlook.com>
2014-04-27 10:21:17 +0800

    setup fake object factory for testing

---
+++ MySocial.Test/FakeObjectFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+
+namespace MySocial.Test
+{
+    internal static class FakeObjectFactory
+    {
+        private static readonly Dictionary<Type, object> Container = new Dictionary<Type, object>();
+
+        static FakeObjectFactory()
+        {
+            Container.Add(typeof(IApplicationSettings), FakeSettings.GetFake());
+        }
+
+        internal static T Get<T>()
+        {
+            var obj = Container[typeof (T)];
+
+            return obj == null ? default(T) : (T) obj;
+        }
+    }
+}
---
+++ MySocial.Test/FakeSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+
+namespace MySocial.Test
+{
+    internal class FakeSettings
+    {
+        internal static IApplicationSettings GetFake()
+        {
+            var appSettings = A.Fake<IApplicationSettings>();
+
+            A.CallTo(() => appSettings.FacebookAppKey).Returns("540740926047667");
+            A.CallTo(() => appSettings.FacebookAppSecret).Returns("80508b188cfe0de0dfd51b1c9909ec0f");
+            A.CallTo(() => appSettings.TwitterAppKey).Returns("lJbyz3I2dkEhBOsi2jqibg6Oh");
+            A.CallTo(() => appSettings.TwitterAppSecret).Returns("LG453b9DaFReCbHa8t3ty5DPT4wLqBBeuxkqjQAJp1tTkFyGlz");
+
+            return appSettings;
+        }
+    }
+}
