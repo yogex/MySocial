2ce017f7abd07009bf8407edf5f3a2492bf8962d
Benedictus Yoga <yogex@outlook.com>
2014-04-27 10:20:28 +0800

    refactor extract interface

---
+++ MySocial/IApplicationSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace MySocial
+{
+    public interface IApplicationSettings
+    {
+        string FacebookAppKey { get; }
+
+        string FacebookAppSecret { get; }
+
+        string TwitterAppKey { get; }
+
+        string TwitterAppSecret { get; }
+    }
+}
